ca5e606d9fefe79abb41431387b451509e0d89d9
Артем Кухар <ipzk241_kaa@student.ztu.edu.ua>
2025-01-29 13:47:34 +0200

    Додавання програми та файлу .gitignore

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Введіть числа для додавання");
+        Console.Write("Перше число: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Друге число: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int sum = num1 + num2;
+        Console.WriteLine($"Сума числ: {sum}");
+    }
+}
